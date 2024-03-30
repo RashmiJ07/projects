57e5e5f81a519d0bcb1a337ea521fb01c1301414
RashmiJ07 <107768258+RashmiJ07@users.noreply.github.com>
2024-03-31 00:22:37 +0530

    Add files via upload

---
+++ Day 1 - Bandname project.py
@@ -0,0 +1,13 @@
+# Band name generator
+# step 1: add user instructions
+print(f"Welcom to the Band name Generator.")
+
+# step 2: take user inputs
+cityName = input(f"What's the name of the city that you grew up in?\n")
+petName = input(f"What's your first pet name?\n")
+
+# step 4: combine the inputs and create the band name
+BandName = cityName+ ' '+petName
+# step 5: give the output to the user
+print(f"Your Band name is {BandName}")
+
